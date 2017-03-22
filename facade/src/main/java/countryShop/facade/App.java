3dc354a3ccd1bc16d0218d1030c6d47485370dec
1158164461@qq.com <1158164461@qq.com>
2017-03-22 12:49:27 +0800

    add

---
+++ facade/src/main/java/countryShop/facade/App.java
@@ -0,0 +1,13 @@
+package countryShop.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
