0eca69f57543b250b840949361736806d50524fa
lishon <lishon@192.168.1.114>
2017-03-29 16:46:45 +0800

    rest dubbo mapresources

--- facade/src/main/java/com/cs/controller/impl/UserControllerImpl.java
+++ facade/src/main/java/com/cs/controller/impl/UserControllerImpl.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.cs.controller.UserController;
 
 
 @Controller
 @RequestMapping("/user")
+@Service(interfaceClass=UserController.class, protocol = {"rest", "dubbo"})
 public class UserControllerImpl  implements UserController{
 	@RequestMapping("getUsers")
 	@ResponseBody
