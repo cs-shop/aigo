632209ce6d0f25f54bce385daced4cb016cbd37c
lishon <lishon@192.168.1.114>
2017-03-31 09:11:55 +0800

    javax-servlet3.0

--- service/src/main/java/com/cs/controller/impl/UserControllerImpl.java
+++ service/src/main/java/com/cs/controller/impl/UserControllerImpl.java
@@ -1,9 +1,5 @@
 package com.cs.controller.impl;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.cs.controller.UserController;
 
 
