022382135622fef4be0e37c92939feed52ad5303
1158164461@qq.com <1158164461@qq.com>
2017-04-10 14:14:42 +0800

    f

--- facade/src/main/java/com/cs/controller/impl/UserControllerImpl.java
+++ facade/src/main/java/com/cs/controller/impl/UserControllerImpl.java
@@ -25,7 +25,7 @@ public class UserControllerImpl  implements UserController{
 	@ResponseBody
 	public String login(@PathVariable("id") long id) {
 		User user = userService.selectByPrimaryKey(id);
-		System.out.println("fff");
+		System.out.println("fffd");
 		return JSON.toJSONString(user);
 	}
 
