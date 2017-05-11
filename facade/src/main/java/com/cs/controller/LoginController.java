7e88322c9459fec7c712f56283c7d9b73c6264b3
lei-fengli <lileifeng1127@163.com>
2017-05-11 09:16:14 +0800

    ii

--- facade/src/main/java/com/cs/controller/LoginController.java
+++ facade/src/main/java/com/cs/controller/LoginController.java
@@ -1,5 +1,31 @@
 package com.cs.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/file")
 public class LoginController {
-	
+	@RequestMapping("/getFile")
+	public ResponseEntity<byte[]> download(HttpServletRequest request) throws IOException {
+	    File file = new File("C://Users//lishon//Desktop//1.jpg");
+	    byte[] body = null;
+	    InputStream is = new FileInputStream(file);
+	    body = new byte[is.available()];
+	    is.read(body);
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.add("Content-Disposition", "attchement;filename=" + file.getName());
+	    HttpStatus statusCode = HttpStatus.OK;
+	    ResponseEntity<byte[]> entity = new ResponseEntity<byte[]>(body, headers, statusCode);
+	    return entity;
+	}
 }
